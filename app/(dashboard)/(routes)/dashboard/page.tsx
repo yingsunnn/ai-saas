fc0f003446eedf37007e4dbcc4d5885c2449a36f
yingsunnn <sco.suen@gmail.com>
2023-11-26 01:21:18 -0500

    note

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+const DashboardPage = () => {
+  return (
+    <>
+      <p className="text-6xl text-green-500">Hello home page</p>
+      <Button variant="destructive">Click Me</Button>
+    </>
+  );
+}
+
+export default DashboardPage;
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,10 @@
+const LandingPage = () => {
+  return (
+    <div>
+
+      Landing Page 
+    </div>
+  );
+}
+
+export default LandingPage;
