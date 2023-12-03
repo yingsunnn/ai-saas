8452d7840a9b9f7cc84275486ec06d6bdd5937fd
yingsunnn <sco.suen@gmail.com>
2023-12-03 14:01:25 -0500

    Finished landing page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,18 +1,14 @@
-import { Button } from "@/components/ui/button";
+import { LandingContent } from "@/components/landing-content";
+import { LandingHero } from "@/components/landing-hero";
+import { LandingNavbar } from "@/components/landing-navbar";
 import Link from "next/link";
 
 const LandingPage = () => {
   return (
-    <div>
-      Landing Page
-      <div>
-        <Link href="/sign-in">
-          <Button>Login</Button>
-        </Link>
-        <Link href="/sign-up">
-          <Button>Register</Button>
-        </Link>
-      </div>
+    <div className="h-full ">
+      <LandingNavbar />
+      <LandingHero />
+      <LandingContent />
     </div>
   );
 };
